5193b42850ff3b37841cab46af3029f8b0625b2e
Marcio de Andrade <marcio@requestdev.net>
2024-04-25 09:30:16 -0400

    Instalação dependência Dapper e Criação da classe Globals com o Path para o arquivo conn.xml

---
+++ DapperRepoWinForm/Utilities/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperRepoWinForm.Utilities
+{
+    public static class Globals
+    {
+        public static String stringConn = "";
+        public const string Path = "D:\\conn.xml";
+    }
+}
